cfde6f30e9bafe6e52697d7fc412f86638a00667
Kira <me@imkira.com>
2017-09-11 22:47:05 +0800

    feat(store): add informer store

---
+++ pkg/store/store.go
@@ -0,0 +1,54 @@
+package store
+
+import (
+	"fmt"
+	"sync"
+
+	"github.com/caicloud/release-controller/pkg/kube"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/client-go/informers"
+	"k8s.io/client-go/tools/cache"
+)
+
+type IntegrationStore interface {
+	InformerFor(gvk schema.GroupVersionKind) (informers.GenericInformer, error)
+}
+
+type integrationStore struct {
+	lock      sync.RWMutex
+	resources kube.APIResources
+	factory   informers.SharedInformerFactory
+	informers map[schema.GroupVersionKind]informers.GenericInformer
+	stopCh    <-chan struct{}
+}
+
+func NewIntegrationStore(resources kube.APIResources, factory informers.SharedInformerFactory, stopCh <-chan struct{}) IntegrationStore {
+	return &integrationStore{
+		resources: resources,
+		factory:   factory,
+		informers: make(map[schema.GroupVersionKind]informers.GenericInformer),
+		stopCh:    stopCh,
+	}
+}
+
+func (is *integrationStore) InformerFor(gvk schema.GroupVersionKind) (informers.GenericInformer, error) {
+	is.lock.Lock()
+	defer is.lock.Unlock()
+	gi, ok := is.informers[gvk]
+	if !ok {
+		resource, err := is.resources.ResourceFor(gvk)
+		if err != nil {
+			return nil, err
+		}
+		gi, err = is.factory.ForResource(resource.GroupVersionResource())
+		if err != nil {
+			return nil, err
+		}
+		is.informers[gvk] = gi
+		is.factory.Start(is.stopCh)
+		if !cache.WaitForCacheSync(is.stopCh, gi.Informer().HasSynced) {
+			return nil, fmt.Errorf("can't sync informer for: %s", gvk)
+		}
+	}
+	return gi, nil
+}
