3a47849b1d1c921e3e7cfec0d4b612fc1e0436f5
Kira <me@imkira.com>
2017-09-13 18:55:45 +0800

    feat(kube): options can specify multiple owner references now

--- pkg/kube/options.go
+++ pkg/kube/options.go
@@ -7,7 +7,7 @@ type GetOptions struct {
 	// OwnerReference confirms the desired owner for resources.
 	// If the option is nil, it means that all matched object
 	// are allowed, even if they have different owners.
-	OwnerReference *metav1.OwnerReference
+	OwnerReferences []metav1.OwnerReference
 	// IgnoreNonexistence ignores resources which is not found.
 	// It won't return an error when a desired resource does
 	// not exist.
@@ -16,20 +16,20 @@ type GetOptions struct {
 
 // CreateOptions is a group options for creating resources
 type CreateOptions struct {
-	// OwnerReference desides the owner for created resources.
-	OwnerReference *metav1.OwnerReference
+	// OwnerReference desides owners to create resources.
+	OwnerReferences []metav1.OwnerReference
 }
 
 // UpdateOptions is a  group options for updating resources
 type UpdateOptions struct {
-	// OwnerReference enforces the owner when create/update/
+	// OwnerReferences enforces owners when create/update/
 	// delete resources in an update operation.
-	OwnerReference *metav1.OwnerReference
+	OwnerReferences []metav1.OwnerReference
 }
 
 // DeleteOptions is a  group options for deleting resources
 type DeleteOptions struct {
-	// OwnerReference is used to make sure that all deleted
-	// resources are belong to the owner.
-	OwnerReference *metav1.OwnerReference
+	// OwnerReferences is used to make sure that all deleted
+	// resources are belong to these owners.
+	OwnerReferences []metav1.OwnerReference
 }
