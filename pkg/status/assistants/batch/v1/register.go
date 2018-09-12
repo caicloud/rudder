8e42f4abc49c32ed255591347164ffd21609b12b
Jim Zhang <jim.zhang@caicloud.io>
2018-09-12 14:43:34 +0800

    feat(status): move pkg/status to rudder

--- pkg/status/assistants/batch/v1/register.go
+++
@@ -1,8 +0,0 @@
-package v1
-
-import "github.com/caicloud/rudder/pkg/status"
-
-// Assist adds the list of known assistant funcs to umpire
-func Assist(umpire status.Umpire) {
-	umpire.Employ(gvkJob, JobAssistant)
-}
