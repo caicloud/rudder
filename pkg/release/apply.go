325888ef6492b4f53e6b80c466953a924ec8a2d7
Jim Zhang <jim.zhang@caicloud.io>
2018-10-23 11:56:29 +0800

    refactor(apply): always try to calculate the right version

--- pkg/release/apply.go
+++ pkg/release/apply.go
@@ -1,6 +1,7 @@
 package release
 
 import (
+	"fmt"
 	"reflect"
 
 	releaseapi "github.com/caicloud/clientset/pkg/apis/release/v1alpha1"
@@ -8,7 +9,8 @@ import (
 	"github.com/caicloud/rudder/pkg/render"
 	"github.com/caicloud/rudder/pkg/storage"
 	"github.com/golang/glog"
-	"k8s.io/apimachinery/pkg/api/errors"
+	core "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 )
 
 // applyRelease wouldn't delete anything. It leaves all antiquated resources to GC. So GC should take
@@ -29,61 +31,94 @@ func (rc *releaseContext) applyRelease(backend storage.ReleaseStorage, release *
 		manifests = render.SplitManifest(rel.Status.Manifest)
 	} else {
 		glog.V(4).Infof("Apply release %s/%s", release.Namespace, release.Name)
-		var history *releaseapi.ReleaseHistory
-		if release.Status.Version > 0 {
-			var err error
-			history, err = backend.History(release.Status.Version)
-			if err != nil && !errors.IsNotFound(err) {
-				glog.Errorf("Failed to get release hisory for %s/%s: %v", release.Namespace, release.Name, err)
-				return recordError(backend, err)
-			}
+
+		// current max_version correct_version changed     nextVersion
+		// 0       0            1               true/false  1
+		// 0       2            2               true        3
+		// 0       2            2               false       2
+		// 1       2            1               false       1
+		// 1       2            1               true        3
+		// 2       2            2               true        3
+		// 2       2            2               false       2
+		// 3       2            2               true        3
+		// 3       2            2               false       2
+
+		// calculate version
+		var correctedVersion, nextVersion int32
+
+		var err error
+		// find the history with version
+		// get the history with biggest version
+		histories, err := backend.Histories()
+		if err != nil {
+			glog.Errorf("Failed to get histories for release %s/%s: %v", release.Namespace, release.Name, err)
+			return recordError(backend, err)
 		}
-		if history != nil &&
-			history.Spec.Config == release.Spec.Config &&
-			reflect.DeepEqual(history.Spec.Template, release.Spec.Template) {
-			glog.V(4).Infof("Release %s/%s has no updates", release.Namespace, release.Name)
-			// No updates.
-			manifests = render.SplitManifest(history.Spec.Manifest)
+
+		if len(histories) == 0 {
+			correctedVersion = 1
+			nextVersion = 1
 		} else {
-			// Update.
-			histories, err := backend.Histories()
-			if err != nil {
-				glog.Errorf("Failed to get histories for release %s/%s: %v", release.Namespace, release.Name, err)
-				return recordError(backend, err)
-			}
-			if len(histories) > 0 {
-				latestHistory := histories[0]
-				if latestHistory.Spec.Config == release.Spec.Config &&
-					reflect.DeepEqual(latestHistory.Spec.Template, release.Spec.Template) {
-					release.Status.Version = latestHistory.Spec.Version
-				} else {
-					release.Status.Version = latestHistory.Spec.Version + 1
+			var currentHistory *releaseapi.ReleaseHistory
+			latestHistory := &histories[0]
+			latestVersion := latestHistory.Spec.Version
+			changed := false
+
+			// Assuming that the latest history is current history
+			currentHistory = latestHistory
+			// try to find out the real history
+			for _, h := range histories {
+				if h.Spec.Version == release.Status.Version {
+					currentHistory = &h
+					break
 				}
+			}
+
+			if release.Spec.Config != currentHistory.Spec.Config ||
+				!reflect.DeepEqual(release.Spec.Template, currentHistory.Spec.Template) {
+				changed = true
+			}
+
+			correctedVersion = currentHistory.Spec.Version
+
+			if !changed {
+				// nothing changed, nextVersion is correctedVersion
+				nextVersion = correctedVersion
 			} else {
-				release.Status.Version = 1
+				// if somthing changed, the nextVersion always be latestVersion + 1
+				nextVersion = latestVersion + 1
 			}
-			// Render
-			carrier, err := rc.render.Render(&render.RenderOptions{
-				Namespace: release.Namespace,
-				Release:   release.Name,
-				Version:   release.Status.Version,
-				Template:  release.Spec.Template,
-				Config:    release.Spec.Config,
-			})
-			if err != nil {
-				// Record error status
-				glog.Errorf("Failed to render release %s/%s: %v", release.Namespace, release.Name, err)
-				return recordError(backend, err)
-			}
-			manifests = carrier.Resources()
-			release.Status.Manifest = render.MergeResources(manifests)
-			glog.V(4).Infof("Update release %s/%s for version %d", release.Namespace, release.Name, release.Status.Version)
-			_, err = backend.Update(release)
-			if err != nil {
-				glog.Errorf("Failed to update release %s/%s: %v", release.Namespace, release.Name, err)
-				return recordError(backend, err)
-			}
+
 		}
+
+		release.Status.Version = nextVersion
+
+		// check the manifests
+
+		// Render
+		carrier, err := rc.render.Render(&render.RenderOptions{
+			Namespace: release.Namespace,
+			Release:   release.Name,
+			Version:   release.Status.Version,
+			Template:  release.Spec.Template,
+			Config:    release.Spec.Config,
+		})
+		if err != nil {
+			// Record error status
+			glog.Errorf("Failed to render release %s/%s: %v", release.Namespace, release.Name, err)
+			return recordError(backend, err)
+		}
+
+		manifests = carrier.Resources()
+		release.Status.Manifest = render.MergeResources(manifests)
+
+		glog.V(4).Infof("Update manifest of release %s/%s for version %d", release.Namespace, release.Name, release.Status.Version)
+		_, err = backend.Update(release)
+		if err != nil {
+			glog.Errorf("Failed to update release %s/%s: %v", release.Namespace, release.Name, err)
+			return recordError(backend, err)
+		}
+
 	}
 	// Apply resources.
 	if err := rc.client.Apply(release.Namespace, manifests, kube.ApplyOptions{
@@ -98,5 +133,47 @@ func (rc *releaseContext) applyRelease(backend storage.ReleaseStorage, release *
 		return err
 	}
 	glog.V(4).Infof("Applied release %s/%s for version %d", release.Namespace, release.Name, release.Status.Version)
+	return nil
+}
+
+// ignore checks if an object should be ignored.
+func (rc *releaseContext) ignore(obj runtime.Object) bool {
+	for _, i := range rc.ignored {
+		if i == obj.GetObjectKind().GroupVersionKind() {
+			return true
+		}
+	}
+	return false
+}
+
+// fix modifies new object.
+func (rc *releaseContext) fix(origin, new, current runtime.Object) error {
+	switch origin.GetObjectKind().GroupVersionKind() {
+	case core.SchemeGroupVersion.WithKind("Service"):
+		// Fix service when convert NodePort to ClusterIP.
+		o, ok := origin.(*core.Service)
+		if !ok {
+			return fmt.Errorf("can't convert origin object %v to service", origin.GetObjectKind())
+		}
+		n, ok := new.(*core.Service)
+		if !ok {
+			return fmt.Errorf("can't convert new object %v to service", new.GetObjectKind())
+		}
+		c, ok := current.(*core.Service)
+		if !ok {
+			return fmt.Errorf("can't convert current object %v to service", current.GetObjectKind())
+		}
+		if o.Spec.Type == core.ServiceTypeNodePort && n.Spec.Type == core.ServiceTypeClusterIP {
+			// Set NodePort for origin object
+			for i := 0; i < len(o.Spec.Ports) && i < len(c.Spec.Ports); i++ {
+				o.Spec.Ports[i].NodePort = c.Spec.Ports[i].NodePort
+			}
+			// Clear NodePort for new object
+			ports := n.Spec.Ports
+			for i := range ports {
+				ports[i].NodePort = 0
+			}
+		}
+	}
 	return nil
 }
