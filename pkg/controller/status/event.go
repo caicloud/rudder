ce607bef2efd0a46f79029815d5c6c94168c76e5
Jim Zhang <jim.zhang@caicloud.io>
2018-12-28 12:37:59 +0800

    fix: sync event with statefulset and daemenset

--- pkg/controller/status/event.go
+++ pkg/controller/status/event.go
@@ -39,23 +39,33 @@ func (sc *StatusController) enqueueEvent(obj interface{}) {
 	if !ok {
 		return
 	}
-	if event.InvolvedObject.Kind != "Pod" &&
-		event.InvolvedObject.Kind != "ReplicaSet" {
+	factory := sc.store.SharedInformerFactory()
+	var accessor metav1.ObjectMetaAccessor
+	var err error
+	switch event.InvolvedObject.Kind {
+	case "Pod":
+		accessor, err = factory.Core().V1().Pods().Lister().Pods(event.InvolvedObject.Namespace).Get(event.InvolvedObject.Name)
+		if err != nil {
+			return
+		}
+	case "ReplicaSet":
+		accessor, err = factory.Apps().V1().ReplicaSets().Lister().ReplicaSets(event.InvolvedObject.Namespace).Get(event.InvolvedObject.Name)
+		if err != nil {
+			return
+		}
+	case "StatefulSet":
+		accessor, err = factory.Apps().V1().StatefulSets().Lister().StatefulSets(event.InvolvedObject.Namespace).Get(event.InvolvedObject.Name)
+		if err != nil {
+			return
+		}
+	case "DaemonSet":
+		accessor, err = factory.Apps().V1().DaemonSets().Lister().DaemonSets(event.InvolvedObject.Namespace).Get(event.InvolvedObject.Name)
+		if err != nil {
+			return
+		}
+	default:
 		return
 	}
-	factory := sc.store.SharedInformerFactory()
-	if event.InvolvedObject.Kind == "Pod" {
-		pod, err := factory.Core().V1().Pods().Lister().Pods(event.InvolvedObject.Namespace).Get(event.InvolvedObject.Name)
-		if err != nil {
-			return
-		}
-		sc.enqueueAccessor(pod)
-	} else if event.InvolvedObject.Kind == "ReplicaSet" {
-		rs, err := factory.Apps().V1().ReplicaSets().Lister().ReplicaSets(event.InvolvedObject.Namespace).Get(event.InvolvedObject.Name)
-		if err != nil {
-			return
-		}
-		sc.enqueueAccessor(rs)
-	}
 
+	sc.enqueueAccessor(accessor)
 }
