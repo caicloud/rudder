08fa171d21d09ed98584efc6661930a9ad684d15
Zhizhen He <hezhizhen@caicloud.io>
2019-08-05 17:02:41 +0800

    refactor(storage): use one unified function for conditions

--- pkg/storage/condition.go
+++ pkg/storage/condition.go
@@ -6,6 +6,35 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
+type Reason string
+
+const (
+	Available   Reason = "Available"
+	Failure     Reason = "Failure"
+	Creating    Reason = "Creating"
+	Updating    Reason = "Updating"
+	Rollbacking Reason = "Rollbacking"
+)
+
+// Condition returns a release condition based on given reason.
+func Condition(reason Reason, msg string) releaseapi.ReleaseCondition {
+	ret := releaseapi.ReleaseCondition{
+		Status:             core.ConditionTrue,
+		LastTransitionTime: metav1.Now(),
+		Message:            msg,
+		Reason:             string(reason),
+	}
+	switch reason {
+	case Available:
+		ret.Type = releaseapi.ReleaseAvailable
+	case Failure:
+		ret.Type = releaseapi.ReleaseFailure
+	case Creating, Updating, Rollbacking:
+		ret.Type = releaseapi.ReleaseProgressing
+	}
+	return ret
+}
+
 // Reasons for releases
 const (
 	ReasonAvailable   = "Available"
